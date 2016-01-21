c9ed882fe2562905c5f4e4b5afdf25e1df8f4d78
Gabriel Pérez <gabriel@garox.org>
2016-01-21 15:18:48 -0300

    youtube style player

--- client/script.js
+++ client/script.js
@@ -30,15 +30,21 @@ Template.videoTemp.events({
 	"click #mute" : function () {
 		var video = document.getElementById("video");
 		var muteButton = document.getElementById("mute");
-		if (video.muted == false) {
+		if (video.volume != 0) {
 			// Mute the video
-			video.muted = true;
+			var video = document.getElementById("video");
+			var volumeBar = document.getElementById("volume-bar");
+			volumeBar.value = 0;
+			video.volume = volumeBar.value;
 
 			// Update the button text
 			muteButton.innerHTML = "Unmute";
 		} else {
 			// Unmute the video
-			video.muted = false;
+			var video = document.getElementById("video");
+			var volumeBar = document.getElementById("volume-bar");
+			volumeBar.value = 1;
+			video.volume = volumeBar.value;
 
 			// Update the button text
 			muteButton.innerHTML = "Mute";
@@ -118,6 +124,20 @@ Template.videoTemp.events({
 	 stoppedElement.classList.remove("fadeOutDown");
 	 videoContainer.classList.remove("no-cursor");
   }
+});
+
+Template.videoTemp.onCreated(function () {
+	var client = new WebTorrent();
+	var torrentId = "magnet:?xt=urn:btih:6a9759bffd5c0af65319979fb7832189f4f3c35d";
+	var torrentDiv = "#video";
+
+	client.add(torrentId, function (torrent) {
+		// Torrents can contain many files. Let's use the first.
+		var file = torrent.files[0]
+
+		// Display the file by adding it to the DOM. Supports video, audio, image, etc. files
+		file.renderTo(torrentDiv)
+	});
 });
 
 // // Pause the video when the seek handle is being dragged
